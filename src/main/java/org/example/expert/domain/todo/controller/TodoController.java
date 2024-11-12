0650f6232d84f429ad47e3e4d50cc0a1a4d5c136
TIP_Santa <bhb89@naver.com>
2024-11-12 15:19:31 +0900

    Level 1. 코드 개선 퀴즈 - @Transactional 의 이해

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -10,6 +10,7 @@ import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +19,9 @@ public class TodoController {
 
     private final TodoService todoService;
 
+    // Level 1. 코드 개선 퀴즈 - @Transactional 의 이해
+    // /todos를 호출했을 때 오류가 발생해야 하는데 오류가 발생하지 않아 오류를 확인하지 못했습니다.
+    @Transactional
     @PostMapping("/todos")
     public ResponseEntity<TodoSaveResponse> saveTodo(
             @Auth AuthUser authUser,
